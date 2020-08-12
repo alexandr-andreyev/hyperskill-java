adcc7371f34217e1eb30dc3da6cf08e0440b58a8
Alex.techlinekz <52343853+alextechlinekz@users.noreply.github.com>
2020-08-12 22:12:34 +0600

    Update Main.java
    3/6 correct

--- Projects/Encryption-Decryption/Main.java
+++ Projects/Encryption-Decryption/Main.java
@@ -1,7 +1,39 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
+    public static char[] encode(char[] charsFromText,int key) {
+        for (int i = 0; i < charsFromText.length; i++) {
+            charsFromText[i] += key;
+        }
+        return charsFromText;
+    }
+
+    public static char[] decode(char[] charsFromText,int key) {
+        for (int i = 0; i < charsFromText.length; i++) {
+            charsFromText[i] -= key;
+        }
+        return charsFromText;
+    }
+
     public static void main(String[] args) {
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String text = scanner.nextLine();
+        int key = scanner.nextInt();
+        char[] charsFromText = text.toCharArray();
+        switch (command) {
+            case "enc" :
+            System.out.println(encode(charsFromText,key));
+            break;
+            case "dec" :
+            System.out.println(decode(charsFromText,key));
+            break;
+            default:
+                break;
+        }
+
     }
 }
+
