5d41b6199ce632a87f8e8a2748a830726488ce23
Alex.techlinekz <52343853+alextechlinekz@users.noreply.github.com>
2020-08-13 16:12:38 +0600

    Update Main.java
    4/6 correct

--- Projects/Encryption-Decryption/Main.java
+++ Projects/Encryption-Decryption/Main.java
@@ -1,6 +1,6 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static char[] encode(char[] charsFromText,int key) {
@@ -18,10 +18,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String command = scanner.nextLine();
-        String text = scanner.nextLine();
-        int key = scanner.nextInt();
+        String command = "enc";
+        String text = "";
+        int key = 0;
+
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode")) {
+                command = args[i + 1];
+            } else if (args[i].equals("-key")) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if (args[i].equals("-data")) {
+                text = args[i + 1];
+            }
+        }
         char[] charsFromText = text.toCharArray();
         switch (command) {
             case "enc" :
@@ -36,4 +45,3 @@ public class Main {
 
     }
 }
-
