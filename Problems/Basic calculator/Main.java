dcdce4a9d7657d0a55aadf643fc88ee51894ea50
Alex.techlinekz <52343853+alextechlinekz@users.noreply.github.com>
2020-08-13 13:58:03 +0600

    Create Main.java

---
+++ Problems/Basic calculator/Main.java
@@ -0,0 +1,21 @@
+class Problem {
+
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[1]);
+        int num2 = Integer.parseInt(args[2]);
+        switch (args[0]) {
+            case "+" :
+                System.out.println(num1 + num2);
+                break;
+            case  "-" :
+                System.out.println(num1 - num2);
+                break;
+            case "*" :
+                System.out.println(num1 * num2);
+                break;
+            default :
+                System.out.println("Unknown operator");
+                break;
+        }
+    }
+}
