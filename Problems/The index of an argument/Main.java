6bf2229ecad5432dba8c0ea2479eecef7a25a037
Alex.techlinekz <52343853+alextechlinekz@users.noreply.github.com>
2020-08-13 15:31:55 +0600

    Create Main.java

---
+++ Problems/The index of an argument/Main.java
@@ -0,0 +1,12 @@
+class Problem {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("test")) {
+                System.out.println(i);
+                break;
+            } else if (i == args.length - 1){
+                System.out.println("-1");
+            }
+        }
+    }
+}
